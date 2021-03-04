f45e53086e3ad9ddcd3188ba878d11d94188ca02
thodkatz <thodkatz@gmail.com>
2021-03-04 14:40:16 +0200

    Init commit
    dwmblocks inspired by distrotube and torrinfail  modifying Makefile and scripts path make it simpler

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,18 @@
 //Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
+	/*Icon*/	/*Command*/	 	/*Update Interval*/	/*Update Signal*/
+    {" 🐧 ", "~/repos/dwmblocks/scripts/kernel",		    360,		        2},
 
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+	{" 🔺 ", "~/repos/dwmblocks/scripts/upt",		        60,		            2},
+
+	{" 📦 ", "~/repos/dwmblocks/scripts/pacupdate",		360,		        9},
+	
+	{" 💻 ", "~/repos/dwmblocks/scripts/memory",	        6,		            1},
+
+	{" 🔊 ", "~/repos/dwmblocks/scripts/volume",			2,		            10},
+
+	{" 🕑 ", "~/repos/dwmblocks/scripts/clock",			5,		            0},
 };
 
 //sets delimeter between status commands. NULL character ('\0') means no delimeter.
-static char delim[] = " | ";
-static unsigned int delimLen = 5;
+static char delim = '|';
